23fa4001cc12a9f7724f44b7065fbec592442fc6
Nitin Chaurasia <gs.nitin@gmail.com>
2014-09-30 12:42:18 -0400

    for loop : parallel broaken into serial

--- test.c
+++ test.c
@@ -1,13 +1,28 @@
 #include <omp.h>
 #include <stdio.h>
+#define MAX 100
 void main ()
 {
+	int n =0;
+//#pragma omp parallel
+//	{
+//	#pragma omp for
+//	for(n=0; n<MAX; ++n)
+//	{
+//	  printf(" %d", n);
+//	}
+//	printf(".\n");
+//	}//End Parallel
 
-	#pragma omp for
-	for(int n=0; n<10; ++n)
-	{
-	  printf(" %d", n);
-	}
+	  int this_thread = omp_get_thread_num(), num_threads = omp_get_num_threads();
+	  int my_start = (this_thread  ) * MAX / num_threads;
+	  int my_end   = (this_thread+1) * MAX / num_threads;
+	  printf("This_threads =  %d\n",this_thread);
+	  printf("Num_threads =  %d\n",num_threads);
+	  printf("my_start =  %d\n",my_start);
+	  printf("my_end =  %d\n",my_end);
+	  for(n=my_start; n<my_end; ++n)
+	   printf(" %d", n);
 	printf(".\n");
 
 }
