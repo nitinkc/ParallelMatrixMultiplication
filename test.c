5b5f42f4c225ef1c3d78256072bd3d00fa5f0d22
Nitin Chaurasia <nitin@bgsu.edu>
2014-10-01 12:36:31 -0400

    test

--- test.c
+++ test.c
@@ -4,25 +4,22 @@
 void main ()
 {
 	int n =0;
-//#pragma omp parallel
-//	{
-//	#pragma omp for
-//	for(n=0; n<MAX; ++n)
-//	{
-//	  printf(" %d", n);
-//	}
-//	printf(".\n");
-//	}//End Parallel
-
-	  int this_thread = omp_get_thread_num(), num_threads = omp_get_num_threads();
-	  int my_start = (this_thread  ) * MAX / num_threads;
-	  int my_end   = (this_thread+1) * MAX / num_threads;
-	  printf("This_threads =  %d\n",this_thread);
-	  printf("Num_threads =  %d\n",num_threads);
-	  printf("my_start =  %d\n",my_start);
-	  printf("my_end =  %d\n",my_end);
-	  for(n=my_start; n<my_end; ++n)
-	   printf(" %d", n);
+	for(n=0; n<MAX; ++n)
+	{
+	  printf(" %d", n);
+	}
 	printf(".\n");
 
+          int this_thread = omp_get_thread_num(), num_threads = omp_get_num_threads();
+          int my_start = (this_thread  ) * MAX / num_threads;
+          int my_end   = (this_thread+1) * MAX / num_threads;
+          printf("This_threads =  %d\n",this_thread);
+          printf("Num_threads =  %d\n",num_threads);
+          printf("my_start =  %d\n",my_start);
+
+#pragma omp parallel for
+	  
+	for(n=my_start; n<my_end; ++n)
+	   printf(" %d", n);
+	printf(".\n");
 }
